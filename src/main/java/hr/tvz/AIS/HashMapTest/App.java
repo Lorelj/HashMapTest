2135d7e310d3396efcd491095108e9d844485aed
lorelj <lorelj@croz.net>
2015-01-02 17:29:44 +0100

    izmjena testa

--- src/main/java/hr/tvz/AIS/HashMapTest/App.java
+++ src/main/java/hr/tvz/AIS/HashMapTest/App.java
@@ -10,106 +10,113 @@ import com.google.common.base.Stopwatch;
 
 class App {
 
-	private static final TimeUnit TIME_UNIT = TimeUnit.NANOSECONDS;
+  private static final TimeUnit TIME_UNIT = TimeUnit.NANOSECONDS;
 
-	public static void main(String[] args) {
-		System.out.println("HashMap test started");
-		insertionTests();
-		deletionTests();
-		fetchingTests();
-		System.out.println("HashMap test finished");
-	}
+  private static final String CSV_DELIMITER = ";";
 
-	private static void insertionTests() {
-		insertionTest(1000);
-		insertionTest(2000);
-		insertionTest(5000);
-		insertionTest(7500);
-		insertionTest(10000);
-	}
+  public static void main(String[] args) {
+    System.out.println("HashMap test started");
+    insertionTests();
+    deletionTests();
+    fetchingTests();
+    System.out.println("HashMap test finished");
+  }
 
-	private static void fetchingTests() {
-		fetchingTest(1000);
-		fetchingTest(2000);
-		fetchingTest(5000);
-		fetchingTest(7500);
-		fetchingTest(10000);
-	}
+  private static void insertionTests() {
+    PrintWriter fileWriter = null;
+    try {
+      fileWriter = new PrintWriter(new FileWriter("Insertion.csv"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    fileWriter.println("Broj elemenata" + CSV_DELIMITER + "Vrijeme (ns)");
+    fileWriter.println("1000" + CSV_DELIMITER + insertionTest(1000));
+    fileWriter.println("2000" + CSV_DELIMITER + insertionTest(2000));
+    fileWriter.println("5000" + CSV_DELIMITER + insertionTest(5000));
+    fileWriter.println("7500" + CSV_DELIMITER + insertionTest(7500));
+    fileWriter.println("10000" + CSV_DELIMITER + insertionTest(10000));
+    fileWriter.close();
+  }
 
-	private static void deletionTests() {
-		deletionTest(1000);
-		deletionTest(2000);
-		deletionTest(5000);
-		deletionTest(7500);
-		deletionTest(10000);
-	}
+  private static void fetchingTests() {
+    PrintWriter fileWriter = null;
+    try {
+      fileWriter = new PrintWriter(new FileWriter("Fetching.csv"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    fileWriter.println("Broj elemenata" + CSV_DELIMITER + "Vrijeme (ns)");
+    fileWriter.println("1000" + CSV_DELIMITER + fetchingTest(1000));
+    fileWriter.println("2000" + CSV_DELIMITER + fetchingTest(2000));
+    fileWriter.println("5000" + CSV_DELIMITER + fetchingTest(5000));
+    fileWriter.println("7500" + CSV_DELIMITER + fetchingTest(7500));
+    fileWriter.println("10000" + CSV_DELIMITER + fetchingTest(10000));
+    fileWriter.close();
+  }
 
-	public static HashMap<Integer, Integer> fillHashMap(int numOfElements) {
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for (int i = 0; i < numOfElements; i++) {
-			map.put(i, i);
-		}
-		return map;
-	}
+  private static void deletionTests() {
+    PrintWriter fileWriter = null;
+    try {
+      fileWriter = new PrintWriter(new FileWriter("Deletion.csv"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    fileWriter.println("Broj elemenata" + CSV_DELIMITER + "Vrijeme (ns)");
+    fileWriter.println("1000" + CSV_DELIMITER + deletionTest(1000));
+    fileWriter.println("2000" + CSV_DELIMITER + deletionTest(2000));
+    fileWriter.println("5000" + CSV_DELIMITER + deletionTest(5000));
+    fileWriter.println("7500" + CSV_DELIMITER + deletionTest(7500));
+    fileWriter.println("10000" + CSV_DELIMITER + deletionTest(10000));
+    fileWriter.close();
+  }
 
-	public static void insertionTest(int numOfElements) {
-		PrintWriter fileWriter = null;
-		try {
-			fileWriter = new PrintWriter(new FileWriter("Insertion"
-					+ numOfElements + ".csv"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		for (int i = 0; i < 1000; i++) {
-			HashMap<Integer, Integer> map = fillHashMap(numOfElements);
-			Stopwatch stopwatch = Stopwatch.createStarted();
-			map.put(numOfElements + 1, numOfElements + 1);
-			stopwatch.stop();
-			fileWriter.println(stopwatch.elapsed(TIME_UNIT));
-		}
-		fileWriter.close();
-	}
+  public static HashMap<Integer, Integer> fillHashMap(int numOfElements) {
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < numOfElements; i++) {
+      map.put(i, i);
+    }
+    return map;
+  }
 
-	public static void deletionTest(int numOfElements) {
-		PrintWriter fileWriter = null;
+  public static long insertionTest(int numOfElements) {
+    long sum = 0;
+    for (int i = 0; i < 1000; i++) {
+      HashMap<Integer, Integer> map = fillHashMap(numOfElements);
+      Stopwatch stopwatch = Stopwatch.createStarted();
+      map.put(numOfElements + 1, numOfElements + 1);
+      stopwatch.stop();
+      sum += stopwatch.elapsed(TIME_UNIT);
+    }
+    return sum / 1000;
+  }
 
-		try {
-			fileWriter = new PrintWriter(new FileWriter("Deletion"
-					+ numOfElements + ".csv"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		for (int i = 0; i < 1000; i++) {
-			HashMap<Integer, Integer> map = fillHashMap(numOfElements);
-			Stopwatch stopwatch = Stopwatch.createStarted();
-			map.remove((int) (numOfElements * 0.8)); // brise se broj koji je na
-														// 80% npr. za 1000
-														// elemenata obrisat ce
-														// se element sa kljucem 800
-			stopwatch.stop();
-			fileWriter.println(stopwatch.elapsed(TIME_UNIT));
-		}
-		fileWriter.close();
-	}
+  public static long deletionTest(int numOfElements) {
+    long sum = 0;
+    for (int i = 0; i < 1000; i++) {
+      HashMap<Integer, Integer> map = fillHashMap(numOfElements);
+      Stopwatch stopwatch = Stopwatch.createStarted();
+      map.remove((int) (numOfElements * 0.8)); // brise se broj koji je na
+      // 80% npr. za 1000
+      // elemenata obrisat ce
+      // se element sa kljucem 800
+      stopwatch.stop();
+      sum += stopwatch.elapsed(TIME_UNIT);
+    }
+    return sum / 1000;
+  }
 
-	public static void fetchingTest(int numOfElements) {
-		PrintWriter fileWriter = null;
-		try {
-			fileWriter = new PrintWriter(new FileWriter("Fetching"
-					+ numOfElements + ".csv"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		for (int i = 0; i < 1000; i++) {
-			HashMap<Integer, Integer> map = fillHashMap(numOfElements);
-			Stopwatch stopwatch = Stopwatch.createStarted();
-			map.get((int) (numOfElements * 0.8)); // dohvaca se broj koji je na
-													// 80% npr. za 1000
-													// elemenata dohvatit ce se
-													// element sa kljucem 800
-			stopwatch.stop();
-			fileWriter.println(stopwatch.elapsed(TIME_UNIT));
-		}
-		fileWriter.close();
-	}
+  public static long fetchingTest(int numOfElements) {
+    long sum = 0;
+    for (int i = 0; i < 1000; i++) {
+      HashMap<Integer, Integer> map = fillHashMap(numOfElements);
+      Stopwatch stopwatch = Stopwatch.createStarted();
+      map.get((int) (numOfElements * 0.8)); // dohvaca se broj koji je na
+      // 80% npr. za 1000
+      // elemenata dohvatit ce se
+      // element sa kljucem 800
+      stopwatch.stop();
+      sum += stopwatch.elapsed(TIME_UNIT);
+    }
+    return sum / 1000;
+  }
 }
